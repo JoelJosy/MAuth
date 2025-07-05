615512187b2f520f3d8f014920887ac90e8b5c71
Joel Josy <joeljosy79@gmail.com>
2025-07-05 11:14:00 +0400

    setup mongodb

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import { connect } from "mongoose";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -7,6 +9,7 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  await connectToDatabase();
 });
