915f2bab359372993f0bca816f423c6c4ab25055
Joel Josy <joeljosy79@gmail.com>
2025-07-07 19:46:56 +0400

    use error middleware

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ import express from "express";
 import { PORT } from "./config/env.js";
 import { connect } from "mongoose";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+app.use(errorMiddleware); // Last middleware for error handling
+app.get("/", (req, res) => {
+  res.send("Welcome to MAuth API!");
 });
 
 app.listen(PORT, async () => {
